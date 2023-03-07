acc00a8da6e130f4f18b83e4e3b7a83c5ca42bb5
Taimur Mumtaz <taimur.mumtaz0@gmail.com>
2023-03-07 13:27:25 -0500

    Navbar and routing complete

---
+++ src/Components/NavBar/Navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from './NavBarElements'
+
+const Navbar = () => {
+  return (
+      <>
+        <Nav>
+            <NavLink to="/">
+                <h1>LOGO</h1>
+            </NavLink>
+            <Bars />
+            <NavMenu>
+                <NavLink to="/about" activeStyle>
+                    About
+                </NavLink>
+                <NavLink to="/services" activeStyle>
+                    Services
+                </NavLink>
+                <NavLink to="/contact" activeStyle>
+                    Contact
+                </NavLink>
+                <NavLink to="/sign-up" activeStyle>
+                    Sign Up
+                </NavLink>
+            </NavMenu>
+            <NavBtn>
+                <NavBtnLink to="/signIn">Sign In</NavBtnLink>
+            </NavBtn>
+        </Nav>
+      </>
+  ) 
+  
+};
+
+export default Navbar;
